f3a6f00f1037c6558bd741b6f39b246a5b2000a0
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-08-15 17:27:05 +0300

    work on admin dashbord

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
 
-export default Admin
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+    </div>
+  );
+};
+
+export default Admin;
